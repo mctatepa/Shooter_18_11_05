94b9e1f43a4b262b131cfae39b2a2ce0a0d0f5f2
mctatepa <leo.sablong@hetic.net>
2018-11-05 18:00:48 +0100

    :rocket: add score and shoot targets

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,40 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.score = 0
 
 /**
  * Methods
  */
 shooter.addTarget = () =>
 {
+    // Create target
     const $target = document.createElement('div')
     $target.classList.add('target')
     $target.style.top = `${Math.random() * 100}%`
     $target.style.left = `${Math.random() * 100}%`
     shooter.$targets.appendChild($target)
+
+    // Listen to mouse enter
+    $target.addEventListener('mouseenter', () =>
+    {
+        shooter.shootTarget($target)
+    })
+}
+
+shooter.shootTarget = (_$target) =>
+{
+    // Delete target
+    _$target.remove()
+
+    // Add new target
+    shooter.addTarget()
+
+    // Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+
+    // Play sound
 }
 
 shooter.addTarget()
