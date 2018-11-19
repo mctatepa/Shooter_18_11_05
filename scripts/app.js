a2f377fd23ef20a7f9773f5fe75b059da25abc3d
mctatepa <leo.sablong@hetic.net>
2018-11-19 17:45:35 +0100

    :checkered_flag: finish game and highscore

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,18 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.$bestScore = shooter.$container.querySelector('.best-score .value')
 shooter.score = 0
 shooter.secondsLeft = 0
+shooter.bestScore = window.localStorage.getItem('bestScore')
+
+if(shooter.bestScore === null)
+{
+    shooter.bestScore = 0
+}
+
+shooter.bestScore = parseInt(shooter.bestScore)
+shooter.$bestScore.textContent = shooter.bestScore
 
 shooter.sounds = {}
 shooter.sounds.ding = new Audio('ding.mp3')
@@ -43,6 +53,13 @@ shooter.end = () =>
     shooter.$container.classList.add('step-end')
 
     shooter.sounds.finish.play()
+
+    if(shooter.score > shooter.bestScore)
+    {
+        window.localStorage.setItem('bestScore', shooter.score)
+        shooter.bestScore = shooter.score
+        shooter.$bestScore.textContent = shooter.score
+    }
 }
 
 shooter.tick = () =>
