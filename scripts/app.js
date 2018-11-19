26812738370a213167bfdfc6af8a8d82302999ac
mctatepa <leo.sablong@hetic.net>
2018-11-19 17:06:59 +0100

    :penguin: start

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,24 @@ shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.score = 0
 
+shooter.sounds = {}
+shooter.sounds.ding = new Audio('ding.mp3')
+shooter.sounds.finish = new Audio('finish.mp3')
+
+shooter.$start.addEventListener('click', () =>
+{
+    shooter.start()
+})
+
 /**
  * Methods
  */
+shooter.start = () =>
+{
+    shooter.$container.classList.remove('step-start')
+    shooter.$container.classList.add('step-game')
+}
+
 shooter.addTarget = () =>
 {
     // Create target
@@ -42,6 +57,7 @@ shooter.shootTarget = (_$target) =>
     shooter.$score.textContent = shooter.score
 
     // Play sound
+    shooter.sounds.ding.play()
 }
 
 shooter.addTarget()
